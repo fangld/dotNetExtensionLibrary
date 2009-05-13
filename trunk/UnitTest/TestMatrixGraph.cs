ba1e4c1ec220c9622cdfedff71b02c2d7974e584
comun_cp <SND\comun_cp>
2009-05-13 03:11:59 +0000

    +:Finish Dijkstra Function

--- trunk/UnitTest/TestMatrixGraph.cs
+++ trunk/UnitTest/TestMatrixGraph.cs
@@ -10,26 +10,59 @@ namespace UnitTest
     public class TestMatrixGraph
     {
         [Test]
-        public void TestOneEdge()
+        public void TestOneEdgeFloyd()
         {
             MatrixGraph graph = new MatrixGraph(2, 10);
             graph.SetEdge(0, 1, 2);
-            double[][] result = graph.FloydWarshall();
+            double[][] result = graph.Floyd();
             Assert.AreEqual(10, result[1][0]);
             Assert.AreEqual(2, result[0][1]);
         }
 
         [Test]
-        public void TestNormal()
+        public void TestNormalFloyd()
         {
             MatrixGraph graph = new MatrixGraph(3, 10);
             graph.SetEdge(0, 1, 2);
             graph.SetEdge(1, 2, 5);
             graph.SetEdge(0, 2, 6);
-            double[][] result = graph.FloydWarshall();
+            double[][] result = graph.Floyd();
             Assert.AreEqual(2, result[0][1]);
             Assert.AreEqual(6, result[0][2]);
             Assert.AreEqual(5, result[1][2]);
+        }
+
+        [Test]
+        public void TestOneEdgeDijkstra()
+        {
+            MatrixGraph graph = new MatrixGraph(2, 10);
+            graph.SetEdge(0, 1, 2);
+            double[] result = graph.Dijkstra(0);
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(2, result[1]);
+        }
+
+        [Test]
+        public void TestNormalDijkstra()
+        {
+            MatrixGraph graph = new MatrixGraph(3, 10);
+            graph.SetEdge(0, 1, 2);
+            graph.SetEdge(1, 2, 5);
+            graph.SetEdge(0, 2, 6);
+            double[] result = graph.Dijkstra(0);
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(2, result[1]);
+            Assert.AreEqual(6, result[2]);
+
+            result = graph.Dijkstra(1);
+            Assert.AreEqual(10, result[0]);
+            Assert.AreEqual(0, result[1]);
+            Assert.AreEqual(5, result[2]);
+
+            result = graph.Dijkstra(2);
+            Assert.AreEqual(10, result[0]);
+            Assert.AreEqual(10, result[1]);
+            Assert.AreEqual(0, result[2]);
         }
     }
 }
