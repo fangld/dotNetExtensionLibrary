f206ada93a363ef5a258f72b398b242a4b9afd5e
comun_cp <SND\comun_cp>
2009-05-15 08:46:25 +0000

    +:Add EnumerableGenerator class

---
+++ trunk/ExtendLibrary/Algorithms/EnumerableGenerator.cs
@@ -0,0 +1,26 @@
+#if EnumerableGenerator
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtendLibrary.Algorithms
+{
+    public static class EnumerableGenerator
+    {
+        public static IEnumerator<T> GetPowerSet<T>(IEnumerable<T> enumerable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerator<T> GetPermutation<T>(IEnumerable<T> enumerable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerator<T> GetCombination<T>(IEnumerable<T> enumerable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+#endif
