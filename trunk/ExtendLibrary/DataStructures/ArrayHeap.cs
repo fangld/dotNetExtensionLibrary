0bff805fe7ee255b30f7245048ba0f87ec27abfd
comun_cp <SND\comun_cp>
2009-05-15 07:35:34 +0000

    +:Can choose and heap to run dijkstra Function

--- trunk/ExtendLibrary/DataStructures/ArrayHeap.cs
+++ trunk/ExtendLibrary/DataStructures/ArrayHeap.cs
@@ -11,11 +11,6 @@ namespace ExtendLibrary.DataStructures
         #region Fields
 
         /// <summary>
-        /// the count of the items that minheap contains
-        /// </summary>
-        private int count;
-
-        /// <summary>
         /// the count of allocated
         /// </summary>
         private int capacity;
@@ -24,11 +19,6 @@ namespace ExtendLibrary.DataStructures
         /// the array of items
         /// </summary>
         private T[] itemArray;
-
-        /// <summary>
-        /// comparer
-        /// </summary>
-        private readonly MultiComparer<T> comparer;
 
         #endregion
 
@@ -41,13 +31,112 @@ namespace ExtendLibrary.DataStructures
 
         #endregion
 
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ArrayHeap() : this(4)
+        {}
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="capacity">the capacity of stored items</param>
+        public ArrayHeap(int capacity): base(capacity)
+        {
+            this.capacity = capacity;
+            itemArray = new T[capacity];
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="collection">a itemArray that contains items</param>
+        public ArrayHeap(IEnumerable<T> collection)
+            : base(0)
+        {
+            BuildHeap(collection);
+        }
+
+        ///// <summary>
+        ///// Constructor
+        ///// </summary>
+        ///// <param name="collection">a itemArray that contains items</param>
+        ///// <param name="exchangeCallBack">exchange callback</param>
+        //public ArrayHeap(IEnumerable<T> collection, ExchangeCallback exchangeCallBack)
+        //    :base(0, new MultiComparer<T>(), exchangeCallBack)
+        //{
+        //    BuildHeap(collection);
+        //}
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="comparison">comparison that used to compare items</param>
+        public ArrayHeap(Comparison<T> comparison)
+            : this(4, comparison)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="capacity">the capacity of stored items</param>
+        /// <param name="comparison">comparison that used to compare items</param>
+        public ArrayHeap(int capacity, Comparison<T> comparison)
+            : base(capacity, new MultiComparer<T>(comparison), null)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="collection">a itemArray that contains items</param>
+        /// <param name="comparison">comparison that used to compare items</param>        
+        public ArrayHeap(IEnumerable<T> collection, Comparison<T> comparison)
+            : base(collection, new MultiComparer<T>(comparison))
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="comparer">a comparer that implement IComparer that used to compare items</param>
+        public ArrayHeap(IComparer<T> comparer)
+            : this(4, comparer)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="capacity">the capacity of stored items</param>
+        /// <param name="comparer">a comparer that implement IComparer that used to compare items</param>
+        public ArrayHeap(int capacity, IComparer<T> comparer)
+            : base(capacity, new MultiComparer<T>(comparer))
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="collection">a itemArray that contains items</param>
+        /// <param name="comparer">a comparer that implement IComparer that used to compare items</param>
+        public ArrayHeap(IEnumerable<T> collection, IComparer<T> comparer)
+            : base(collection, new MultiComparer<T>(comparer))
+        {
+        }
+
+        #endregion
+
         #region Methods
 
         private int Compare(int xIndex, int yIndex)
         {
             T xItem = itemArray[xIndex];
             T yItem = itemArray[yIndex];
-            return comparer.Compare(xItem, yItem);
+            return Comparer.Compare(xItem, yItem);
         }
 
         /// <summary>
@@ -57,10 +146,10 @@ namespace ExtendLibrary.DataStructures
         /// <param name="yIndex">the index of second item</param>
         protected override void Exchange(int xIndex, int yIndex)
         {
+            base.Exchange(xIndex, yIndex);
             T exchange = itemArray[xIndex];
             itemArray[xIndex] = itemArray[yIndex];
             itemArray[yIndex] = exchange;
-            base.Exchange(xIndex, yIndex);
         }
 
         #endregion
@@ -69,13 +158,13 @@ namespace ExtendLibrary.DataStructures
 
         public override void BuildHeap(IEnumerable<T> collection)
         {
-            count = 0;
+            Count = 0;
             foreach (T item in collection)
             {
-                count++;
+                Count++;
             }
-            capacity = count;
-            itemArray = new T[count];
+            capacity = Count;
+            itemArray = new T[Count];
 
             int index = 0;
             foreach (T item in collection)
@@ -87,19 +176,23 @@ namespace ExtendLibrary.DataStructures
 
         public override void Add(T item)
         {
-            count++;
-            if (count > capacity)
+            Count++;
+            if (Count > capacity)
             {
                 capacity <<= 1;
                 Array.Resize(ref itemArray, capacity);
             }
-            itemArray[count - 1] = item;
+            itemArray[Count - 1] = item;
         }
 
         public override T Peek()
         {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Minheap is empty!");
+            }
             int minIndex = 0;
-            for (int i = 1; i < count; i++)
+            for (int i = 1; i < Count; i++)
             {
                 if (Compare(minIndex, i) > 0)
                 {
@@ -112,8 +205,12 @@ namespace ExtendLibrary.DataStructures
 
         public override T ExtractMin()
         {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Minheap is empty!");
+            }
             int minIndex = 0;
-            for (int i = 1; i < count; i++)
+            for (int i = 1; i < Count; i++)
             {
                 if (Compare(minIndex, i) > 0)
                 {
@@ -121,19 +218,19 @@ namespace ExtendLibrary.DataStructures
                 }
             }
 
-            count--;
+            Count--;
 
-            for (int i = minIndex; i < count; )
+            for (int i = minIndex; i < Count; )
             {
                 Exchange(i, ++i);
             }
 
-            return itemArray[minIndex];
+            return itemArray[Count];
         }
 
         public override void DecreaseKey(int index, T value)
         {
-            if (comparer.Compare(itemArray[index], value) < 0)
+            if (Comparer.Compare(itemArray[index], value) < 0)
             {
                 string message = string.Format("Value {0} is greater than array[{1}]", value, index);
                 throw new InvalidOperationException(message);
