d858af6b9a69b35a8552213c8601cb54bdd5d2b7
comun_cp <SND\comun_cp>
2009-05-19 01:42:03 +0000

    *:Change EnumerableGenerator

--- trunk/ExtendLibrary/Algorithms/EnumerableGenerator.cs
+++ trunk/ExtendLibrary/Algorithms/EnumerableGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ExtendLibrary.Common;
 
 namespace ExtendLibrary.Algorithms
 {
@@ -11,16 +12,69 @@ namespace ExtendLibrary.Algorithms
             throw new NotImplementedException();
         }
 
-#if All
-        public static IEnumerator<T> GetPermutation<T>(IEnumerable<T> enumerable)
+        public static T[] GetArray<T>(IEnumerable<T> collection)
         {
-            throw new NotImplementedException();
+            int count= 0;
+            foreach (T item in collection)
+            {
+                count++;
+            }
+            T[] result = new T[count];
+            int index = 0;
+            foreach (T item in result)
+            {
+                result[index++] = item;
+            }
+            return result;
+        }
+
+        public static void GetPermutation<T>(IEnumerable<T> collection, Action<T[]> action)
+        {
+            MultiComparer<T> comparer = new MultiComparer<T>();
+            T[] array = GetArray(collection);
+            Array.Sort(array);
+            while (true)
+            {
+                action(array);
+                int count = array.Length;
+                int j = count - 2;
+                while (true)
+                {
+                    if (j == -1)
+                        return;
+                    if (comparer.Compare(array[j], array[j+1]) >= 0)
+                    {
+                        j--;
+                    }
+                    else
+                        break;
+                }
+
+                int l = count - 1;
+                while (comparer.Compare(array[j], array[l]) >= 0)
+                {
+                    l--;
+                }
+                T exchange = array[j];
+                array[j] = array[l];
+                array[l] = exchange;
+
+                int k = j + 1;
+                l = count - 1;
+                while (k < l)
+                {
+                    exchange = array[k];
+                    array[k] = array[l];
+                    array[l] = exchange;
+                    k++;
+                    l--;
+                }
+            }
         }
 
         public static IEnumerator<T> GetCombination<T>(IEnumerable<T> enumerable)
         {
             throw new NotImplementedException();
         }
-#endif
     }
 }
