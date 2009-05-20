f0cf9ebf6ee750b854f1b2d91dde83de18e8ab39
comun_cp <SND\comun_cp>
2009-05-20 08:29:21 +0000

    *:Move SortFirstKth to ArrayExtend *:Move Selection to ArrayExtend

--- trunk/ExtendLibrary/Algorithms/ArrayExtend.cs
+++ trunk/ExtendLibrary/Algorithms/ArrayExtend.cs
@@ -5,7 +5,7 @@ using ExtendLibrary.Common;
 
 namespace ExtendLibrary.Algorithms
 {
-    public static class Selection<T>
+    public static class ArrayExtend<T>
     {
         internal static int Patition(T[] array, int left, int right, int pivotIndex, Comparison<T> comparison)
         {
@@ -125,6 +125,80 @@ namespace ExtendLibrary.Algorithms
             Array.Copy(array, index, result, 0, length);
             GetFirstKth(result, comparison, 0, length - 1, k);
             return result;
+        }
+
+        #endregion
+
+        #region Sort first kth item
+
+        private static void SortFirstKth(T[] array, Comparison<T> comparison, int left, int right, int k)
+        {
+            if (array.Length <= 20)
+            {
+                BubbleSortKth(array, comparison, left, right, k);
+            }
+            else
+            {
+                QuickSortFirstKth(array, comparison, left, right, k);
+            }
+        }
+
+        private static void BubbleSortKth(T[] array, Comparison<T> comparison, int left, int right, int k)
+        {
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = left; j <= right - i; j++)
+                {
+                    if (comparison(array[j], array[j + 1]) > 0)
+                    {
+                        T exchange = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = exchange;
+                    }
+                }
+            }
+        }
+
+        private static void QuickSortFirstKth(T[] array, Comparison<T> comparison, int left, int right, int k)
+        {
+            if (right > left)
+            {
+                int pivotIndex = (left + right) >> 1;
+                int pivotNewIndex = Patition(array, left, right, pivotIndex, comparison);
+                QuickSortFirstKth(array, comparison, left, pivotNewIndex - 1, k);
+                if (pivotNewIndex < k)
+                    QuickSortFirstKth(array, comparison, pivotNewIndex + 1, right, k);
+            }
+        }
+
+        public static void SortFirstKth(T[] array, int k)
+        {
+            SortFirstKth(array, 0, array.Length, k);
+        }
+
+        public static void SortFirstKth(T[] array, int k, IComparer<T> comparer)
+        {
+            SortFirstKth(array, 0, array.Length, k, comparer);
+        }
+
+        public static void SortFirstKth(T[] array, int k, Comparison<T> comparison)
+        {
+            SortFirstKth(array, 0, array.Length, k, comparison);
+        }
+
+        public static void SortFirstKth(T[] array, int index, int length, int k)
+        {
+            SortFirstKth(array, index, index + length - 1, k, NativeComparer<T>.Compare);
+        }
+
+        public static void SortFirstKth(T[] array, int index, int length, int k, IComparer<T> comparer)
+        {
+            SortFirstKth(array, index, index + length - 1, k, comparer.Compare);
+        }
+
+        public static void SortFirstKth(T[] array, int index, int length, int k, Comparison<T> comparison)
+        {
+            SortFirstKth(array, comparison, index, index + length - 1, k);
         }
 
         #endregion
