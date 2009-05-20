738928f233716a6100d527a8d48fbe7a11101045
comun_cp <SND\comun_cp>
2009-05-20 03:46:46 +0000

    *:Fix UnitTest issue

--- trunk/UnitTest/TestMixedRadixEnumerator.cs
+++ trunk/UnitTest/TestMixedRadixEnumerator.cs
@@ -10,36 +10,6 @@ namespace UnitTest
     public class TestMixedRadixEnumerator
     {
         [Test]
-        public void TestTwo()
-        {
-            int[] array = new int[] {1, 2};
-            MixedRadixEnumerable<int> mixedRadixEnumerable = new MixedRadixEnumerable<int>(array);
-            foreach (int[] item in mixedRadixEnumerable)
-            {
-                for (int i = 0; i < item.Length; i++)
-                {
-                    Console.Write("{0} ", item[i]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        [Test]
-        public void TestFour()
-        {
-            int[] array = new int[] {1, 2, 3, 4};
-            MixedRadixEnumerable<int> mixedRadixEnumerable = new MixedRadixEnumerable<int>(array);
-            foreach (int[] item in mixedRadixEnumerable)
-            {
-                for (int i = 0; i < item.Length; i++)
-                {
-                    Console.Write("{0} ", item[i]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        [Test]
         public void TestTwoMethod()
         {
             int[] array = new int[] { 1, 2 };
