dc08c1fd71dc8b253bfb87edb6a56eae0155ec60
comun_cp <SND\comun_cp>
2009-05-27 07:47:19 +0000

    +:Add Complex class

---
+++ trunk/ExtensionLibrary/Mathematics/Complex.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionLibrary.Mathematics
+{
+    public class Complex
+    {
+        #region Fields
+
+        private double imag;
+
+        private double real;
+
+        #endregion
+
+        #region Properties
+
+        public double Imag
+        {
+            get { return imag; }
+            set { imag = value; }
+        }
+
+        public double Real
+        {
+            get { return real; }
+            set { real = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Complex()
+        {
+            imag = 0.0;
+            real = 0.0;
+        }
+
+        public Complex(double imag, double real)
+        {
+            this.imag = imag;
+            this.real = real;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static Complex Sin(Complex c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Complex Cos(Complex c)
+        {
+            throw new NotImplementedException();
+            
+        }
+
+        public static Complex Tan(Complex c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Complex Sinh(Complex c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Complex Cosh(Complex c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Complex Tanh(Complex c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Complex Sqrt(Complex c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Complex Exp(Complex c)
+        {
+            throw new NotImplementedException();            
+        }
+
+        public static Complex Pow(Complex x, Complex y)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            if (double.IsNaN(real) || double.IsNaN(imag))
+            {
+                return double.NaN.ToString();
+            }
+
+            if (double.IsNegativeInfinity(real) || double.IsNegativeInfinity(imag))
+            {
+                return double.NegativeInfinity.ToString();
+            }
+
+            if (double.IsPositiveInfinity(real) || double.IsPositiveInfinity(imag))
+            {
+                return double.NegativeInfinity.ToString();
+            }
+
+            if (imag == 0.0)
+            {
+                return real.ToString();
+            }
+            
+            if (real == 0.0)
+            {
+                return imag == 0.0 ? "0" : string.Format("{0}i", imag);
+            }
+
+            return string.Format("{0} + {1}i", real, imag);
+        }
+    }
+}
